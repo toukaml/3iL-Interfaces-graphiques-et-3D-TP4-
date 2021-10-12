9ea8fa16ba92839c708d6e5765539b082665c3a2
Lindsey Toukam <lind.toukam@gmail.com>
2021-10-12 20:20:01 +0200

    INTERFACES GRAPHIQUES ET 3D TP4

---
+++ desktop/src/com/interfacesgraphiqueset3d/tp/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.interfacesgraphiqueset3d.tp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.interfacesgraphiqueset3d.tp.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title="DROP";
+        config.width=800;
+        config.height=480;
+		new LwjglApplication(new Application(), config);
+	}
+}
---
+++ html/src/com/interfacesgraphiqueset3d/tp/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.interfacesgraphiqueset3d.tp.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.interfacesgraphiqueset3d.tp.Application;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+               // return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Application();
+        }
+}
